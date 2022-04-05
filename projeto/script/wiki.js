cd0464d58721f5de58837fff11b954d7555a1b6c
murilochag <murilochagas10@outlook.com>
2022-04-05 15:38:34 -0300

    confirmar

--- projeto/script/wiki.js
+++ projeto/script/wiki.js
@@ -1,4 +1,4 @@
-// personagens *uso de dois metodos*
+// personagens
 const imprimir = document.querySelector('h1')
 
 class Personagem {
@@ -13,19 +13,35 @@ class Personagem {
     }
 }
 
+//classe objetos
+class Obj {
+    constructor(nome, tipo) {
+     this._nome = nome;
+     this._tipo = tipo;
+    }
+    imprimirObj(){
+        return (`Nome: ${this._nome} <br> Tipo: ${this._tipo}`)
+    }
+    
+}
+
+//personagens:
+  //murilo
 const murilo = new Personagem('Murilo','humano','tatooine','jedi')
-const imprimirMurilo = murilo.imprimirPersonagem()
-
+  //paula
 const paula = new Personagem('Paula','humano','naboo','senadora')
-const imprimirPaula = paula.imprimirPersonagem()
 
-//imprimir.innerHTML = imprimirMurilo
+//objetos:
+   //estrela da morte
+const estrelaDaMorte = new Obj('Estrela da morte','arma | estação bélica')
+
 
 var captar = ""
 function chamar (){
     captar = document.getElementById('nome').value;
-    if (captar === "murilo" || captar === "Murilo") { document.getElementById('texto').innerHTML = imprimirMurilo}
-    else if (captar === "paula" || captar === "Paula") {document.getElementById('texto').innerHTML = imprimirPaula
+    if (captar === "murilo" || captar === "Murilo") { document.getElementById('texto').innerHTML = murilo.imprimirPersonagem()}
+    else if (captar === "paula" || captar === "Paula") {document.getElementById('texto').innerHTML = paula.imprimirPersonagem()}
+    else if(captar==="estrela da morte") {document.getElementById('texto').innerHTML = estrelaDaMorte.imprimirObj()
     } else {document.getElementById('texto').innerHTML = "desculpe, nome inválido"}
-    //document.getElementById('texto').innerHTML = imprimirMurilo
+    
 }
