807312250cd9654fa24b58f5a25ca91256bc880c
murilo <murilochagas10@outlook.com>
2022-04-26 11:24:52 -0300

    bbs numerico add

--- javascript/primeiraAvaliacao/bubbleSort.js
+++ javascript/primeiraAvaliacao/bubbleSort.js
@@ -1,4 +1,6 @@
-function bubbleSort(items) {
+// bubble sort com array de string
+
+function bubbleSortSt(items) {
     var length = items.length;  
     for (var i = 0; i < length; i++) { 
         for (var j = 0; j < (length - i - 1); j++) { 
@@ -13,6 +15,35 @@ function bubbleSort(items) {
 
 personagem = ['darth vader','luke skywalker','leia','han solo','c3-po']
 
-bubbleSort(personagem);
+bubbleSortSt(personagem);
 
 console.log(personagem);
+
+
+// bubble sort com array numérico
+
+num = []
+max = 10000
+
+for(let j = 1; j <= max; j++) {
+    num.push(j)
+}
+
+function bubbleSortNum(items) {
+    var length = items.length;  
+    for (var i = 0; i < length; i++) { 
+        for (var j = 0; j < (length - i - 1); j++) { 
+            if(items[j] > items[j+1]) {
+               var tmp = items[j]; 
+               items[j] = items[j+1]; 
+               items[j+1] = tmp; 
+            }
+        }        
+    }
+} 
+
+numeros = [1,6,8,4,3,2,7,9,5,10]
+
+bubbleSortNum(numeros)
+
+console.log(numeros)
