ecbd5e40a20ff74ff391ff7756b86730bd5e57cc
Murilochag <murilochagas10@outlook.com>
2022-05-04 10:33:36 -0300

    conversor bse dec para bin

---
+++ javascript/segundaAvaliacao/pilhas/conver_base_pilha.js
@@ -0,0 +1,61 @@
+class Stack {
+    constructor() {
+      this.items = [];
+    }
+  
+    push(element) {
+      this.items.push(element);
+    }
+  
+    pop() {
+      return this.items.pop();
+    }
+  
+    peek() {
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  
+    clear() {
+      this.items = [];
+    }
+  
+    toArray() {
+      return this.items;
+    }
+  
+    toString() {
+      return this.items.toString();
+    }
+  }
+
+///////////////////////////////////////////////////////////
+
+const pilhaDecToBin = new Stack;
+
+function decTobin(numDec) {
+    let dec = numDec
+    let resto
+    let bin = ''
+
+    while(dec > 0) {
+        resto = Math.floor(dec % 2 ) // resto
+        pilhaDecToBin.push(resto)
+        dec = Math.floor(dec / 2 )
+    }
+
+    while(! pilhaDecToBin.isEmpty()) {
+        bin += (pilhaDecToBin.pop())
+    }
+
+    return console.log(bin)
+}
+
+decTobin(2)
